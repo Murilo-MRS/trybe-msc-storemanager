4a63c21945cb6ec9cb9e2e22eaf82524f96bc8c2
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-13 16:15:27 -0300

    chore: add mysql conection

--- src/models/db/connection.js
+++ src/models/db/connection.js
@@ -1,11 +1,13 @@
+require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3306,
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
-module.exports = connection;
+module.exports = {
+  connection,
+};
