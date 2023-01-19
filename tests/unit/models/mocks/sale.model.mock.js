c7b672b6e77782a8b662226f394771d8cc9b6c86
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-19 10:08:56 -0300

    feat: req 6 - model -  cadastro de venda test

---
+++ tests/unit/models/mocks/sale.model.mock.js
@@ -0,0 +1,9 @@
+const newSale = {
+  id: 1,
+  date: '2023-01-19 12:21:32',
+};
+
+module.exports = {
+  newSale,
+};
+
--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -1,5 +1,24 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const { productModel } = require('../../../src/models');
+const { saleModel } = require('../../../src/models');
 const connection  = require('../../../src/models/db/connection');
-const { allProducts, newProduct, productUpdated } = require('./mocks/product.model.mock');
+const { newSale } = require('./mocks/sale.model.mock');
+
+describe('Teste da camada Product - Model', function () {
+  
+  describe('Cadastra venda', function () {
+    
+    afterEach(function () {
+      sinon.restore();
+    });
+
+    it('com successo', async function () {
+      // Arrange
+      sinon.stub(connection, 'execute').resolves([{ insertId: 1 }]);
+      // Act
+      const result = await saleModel.addNewSale(newSale);
+      // Assert
+      expect(result).to.equal(1);
+    });
+  });
+});
